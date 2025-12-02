4d5fb5818f0620b41ea28d820cb42ba50c855c5f
Lee Heegeon <hptlord@gmail.com>
2025-12-02 23:14:06 +0900

    [Bronze I] Title: ISBN, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/14626. ISBN/ISBN.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String input=br.readLine();
+        int[]stoi=new int[input.length()];
+        int index=0;
+        for(int i=0;i<input.length();i++){
+            char ch=input.charAt(i);
+            if(ch=='*') {
+                index = i;
+                continue;
+            }
+            stoi[i]=ch-'0';
+        }
+        int sum=0;
+        int mul=3;
+        int resultMul=0;
+
+        for(int i=0;i<stoi.length;i++){
+            if(i%2==0) mul=1;
+            else mul=3;
+
+            if(i==stoi.length-1) mul=1;
+
+            if(i==index) {
+                resultMul = mul;
+                continue;
+            }
+
+            sum+=stoi[i]*mul;
+        }
+
+        int result=10-sum%10;
+        if(result==10) result=0;
+        else {
+            while (true) {
+                if (result % resultMul != 0) result += 10;
+                else {
+                    result /= resultMul;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
