8e132b356e8bcc9e7f7d74ab1d43c117848261ea
Lee Heegeon <hptlord@gmail.com>
2026-01-25 16:38:10 +0900

    [level 1] Title: 과일 장수, Time: 79.47 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,21 +1,15 @@
+import java.util.*;
+
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        int score_len=score.length;
-        int[] score_num=new int[score_len+1];
-        
-        for(int i=0;i<score_len;i++){
-            score_num[score[i]]++;
+
+        Arrays.sort(score);
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
         }
-    
-        int index=k;
-        for(int i=0;i<score_len/m;i++){
-            for(int j=0;j<m;j++){
-                while(score_num[index]==0)index--;
-                score_num[index]--;
-            }
-            answer+=index*m;
-        }
+
         return answer;
     }
 }
