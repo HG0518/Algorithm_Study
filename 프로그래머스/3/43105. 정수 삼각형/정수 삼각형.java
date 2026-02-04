fb7d8e03b3d565040deaad835323188b73fd0f16
Lee Heegeon <hptlord@gmail.com>
2026-02-04 23:16:34 +0900

    [level 3] Title: 정수 삼각형, Time: 15.58 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,11 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] triangle) {
-        for(int i=triangle.length-2;i>=0;i--){
-            for(int j=0;j<triangle[i].length;j++){
-                triangle[i][j]=Math.max(triangle[i+1][j]+triangle[i][j],triangle[i+1][j+1]+triangle[i][j]);
+        int[] now;
+        int[] next=new int[]{triangle[0][0]};
+        for(int i=0;i<triangle.length-1;i++){
+            now=next;
+            next=new int[i+2];
+            for(int j=0;j<now.length;j++){
+                inputNext(next,now[j]+triangle[i+1][j],j);
+                inputNext(next,now[j]+triangle[i+1][j+1],j+1);
             }
         }
-        return triangle[0][0];
+        int max=0;
+        for(int i:next){
+            if(max<i)max=i;
+        }
+        return max;
     }
+    
+    public void inputNext(int[] next, int sum, int j){
+        if(next[j]==0) next[j]=sum;
+        else next[j]=next[j]>sum?next[j]:sum;
+    }
+    
 }
